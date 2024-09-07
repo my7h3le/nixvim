8c7a2e5c55a599ec822a30723183592c61e2edcb
Tash <mandate-word-dupe@duck.com>
2024-09-07 02:23:35 -0400

    test explicit defining dependencies type

--- plugins/pluginmanagers/lazy.nix
+++ plugins/pluginmanagers/lazy.nix
@@ -52,7 +52,7 @@ nixvim.neovim-plugin.mkNeovimPlugin {
                 or firenvim for example. (accepts fun(LazyPlugin):boolean)
               '';
 
-              dependencies = helpers.mkNullOrOption (helpers.nixvimTypes.eitherRecursive str listOfPlugins) "Plugin dependencies";
+              dependencies = helpers.mkNullOrOption (types.listOf lazyPluginType) "Plugin dependencies";
 
               init = helpers.mkNullOrLuaFn "init functions are always executed during startup";
 
