0301cbb8293ebe778803092d5a234501d02a0a28
Tash <mandate-word-dupe@duck.com>
2024-09-12 14:59:22 -0400

    progress refactoring tests

--- tests/test-sources/plugins/pluginmanagers/lazy.nix
+++ tests/test-sources/plugins/pluginmanagers/lazy.nix
@@ -24,11 +24,45 @@
     };
   };
 
-  single-package = {
-    plugins.lazy = with pkgs.vimPlugins; {
+  nix-package-plugins = {
+    plugins.lazy = {
       enable = true;
 
-      plugins = [ vim-closer ];
+      plugins = with pkgs.vimPlugins; [
+        # A plugin can be just a nix package
+        vim-closer
+
+        # A plugin can also be an attribute set with `source` set to a nix
+        # package.
+        { source = trouble-nvim; }
+        # `source` can also be a nix store path or path in general.
+        { source = "${telescope-nvim}"; }
+      ];
+    };
+  };
+
+  out-of-tree-plugins = {
+    # Don't run neovim for this test, as it's purely to test module evaluation.
+    test.runNvim = false;
+    plugins.lazy = {
+      enable = true;
+
+      plugins = with pkgs.vimPlugins; [
+        # `source` can also be a short git url of the form `owner/repo`
+        { source = "echasnovski/mini.align"; }
+
+        # `source` can also be a full git url with `http://` or `https://`
+        {
+          source = "https://github.com/nvim-telescope/telescope.nvim";
+          enabled = true;
+          version = false;
+        }
+        {
+          source = "http://github.com/norcalli/nvim-colorizer.lua";
+          enabled = true;
+          version = false;
+        }
+      ];
     };
   };
 
@@ -37,11 +71,9 @@
       enable = true;
 
       plugins = [
-        vim-closer
-
         # Test freeform
         {
-          pkg = trouble-nvim;
+          source = trouble-nvim;
           # The below is not actually a property in the `lazy.nvim` plugin spec
           # but is purely to test freeform capabilities of the `lazyPluginType`.
           blah = "test";
@@ -49,7 +81,7 @@
 
         # Load on specific commands
         {
-          pkg = vim-dispatch;
+          source = vim-dispatch;
           optional = true;
           cmd = [
             "Dispatch"
@@ -61,13 +93,13 @@
 
         # Load on an autocommand event
         {
-          pkg = vim-matchup;
+          source = vim-matchup;
           event = "VimEnter";
         }
 
         # Load on a combination of conditions: specific filetypes or commands
         {
-          pkg = ale;
+          source = ale;
           name = "w0rp/ale";
           ft = [
             "sh"
@@ -87,319 +119,241 @@
 
         # Plugins can have post-install/update hooks
         {
-          pkg = markdown-preview-nvim;
+          source = markdown-preview-nvim;
           cmd = "MarkdownPreview";
         }
 
         # Post-install/update hook with neovim command
         {
-          pkg = nvim-treesitter;
+          source = nvim-treesitter;
           opts = {
             ensure_installed = { };
           };
         }
-
-        # Plugin names can also be passed by themselves so long as they're
-        # defined somewhere else in the `plugins` list.
-        "oil"
-        {
-          name = "oil";
-          pkg = oil-nvim;
-        }
       ];
     };
-  };
-
-  single-dir-only-plugin = {
-    plugins.lazy = with pkgs.vimPlugins; {
-      enable = true;
-      plugins = [ { dir = "${LazyVim}"; } ];
-    };
-  };
-
-  multiple-dir-only-plugins = {
-    plugins.lazy = with pkgs.vimPlugins; {
-      enable = true;
-      plugins = [
-        { dir = "${LazyVim}"; }
-        { dir = "${vim-closer}"; }
-        { dir = "${vim-dispatch}"; }
-      ];
-    };
-  };
-
-  disabling_plugins = {
-    plugins.lazy =
-      with pkgs.vimPlugins;
-      let
-        test_plugin1_path = "${yanky-nvim}";
-        test_plugin2_path = "${whitespace-nvim}";
-      in
-      {
-        enable = true;
-        plugins = [
-          # Enable and then later disable a plugin using it's custom name.
-          {
-            name = "test-mini-nvim";
-            pkg = mini-nvim;
-            enabled = true;
-          }
-          {
-            __unkeyed = "test-mini-nvim";
-            enabled = false;
-          }
-
-          # Enable and then later disable a plugin using `pkg`.
-          {
-            pkg = vim-closer;
-            enabled = true;
-          }
-          {
-            pkg = vim-closer;
-            enabled = false;
-          }
-
-          # Enable plugin using `pkg` and then later disable it using the nix
-          # package's default name.
-          {
-            pkg = vim-dispatch;
-            enabled = true;
-          }
-          {
-            __unkeyed = "vim-dispatch";
-            enabled = true;
-          }
-
-          # Enable a plugin using it's path given to `dir`
-          {
-            dir = test_plugin1_path;
-            # We don't need to specify name to be able to disable it later,
-            # it's just here purely for the sake of the test case.
-            name = "test_plugin1";
-            enabled = true;
-          }
-          # Disable previously enabled test_plugin1 using `dir`.
-          {
-            dir = test_plugin1_path;
-            enabled = false;
-          }
-
-          # Enable a plugin using it's path given to `dir`, but not giving it a
-          # custom name.
-          {
-            dir = test_plugin2_path;
-            enabled = true;
-          }
-          # Disable previously enabled test_plugin2 using `dir`.
-          {
-            dir = test_plugin2_path;
-            enabled = false;
-          }
-        ];
-
-      };
-  };
-
-  plugins-with-dependencies = {
-    plugins.lazy = {
-      enable = true;
-      plugins = with pkgs.vimPlugins; [
-        # Plugins can have dependencies on other plugins
-        {
-          pkg = completion-nvim;
-          optional = true;
-          dependencies = [
-            {
-              pkg = vim-vsnip;
-              optional = true;
-            }
-            {
-              pkg = vim-vsnip-integ;
-              optional = true;
-            }
-          ];
-        }
-
-        # Use dependency and run lua function after load
-        {
-          pkg = nvim-colorizer-lua;
-          dependencies = [ nvim-cursorline ];
-          config = ''
-            function()
-              require("nvim-cursorline").setup{}
-            end '';
-        }
-
-        # Dependencies can be a single package
-        {
-          pkg = LazyVim;
-          dependencies = trouble-nvim;
-        }
-
-        # Dependencies can be multiple packages
-        {
-          pkg = nvim-cmp;
-          dependencies = [
-            cmp-cmdline
-            cmp-vsnip
-          ];
-        }
-
-        # Dependencies can be a single name that is defined elsewhere
-        {
-          pkg = nvim-autopairs;
-          dependencies = "luasnip";
-        }
-        {
-          name = "luasnip";
-          pkg = luasnip;
-        }
-
-        # Dependencies can be a list of names that are defined elsewhere
-        {
-          pkg = nvim-lightbulb;
-          dependencies = [
-            "nvim-lspconfig"
-            "cmp-nvim-lua"
-            "cmp-nvim-lsp"
-          ];
-        }
-        {
-          pkg = nvim-lspconfig;
-          name = "nvim-lspconfig";
-        }
-        {
-          pkg = cmp-nvim-lua;
-          name = "cmp-nvim-lua";
-        }
-        {
-          pkg = cmp-nvim-lsp;
-          name = "cmp-nvim-lsp";
-        }
-
-        # Dependencies can be a list of names that are defined elsewhere in the
-        # list of plugins. If the names given are just the default names of a
-        # package they need not be explicitly defined.
-        {
-          pkg = nui-nvim;
-          dependencies = [
-            "nvim-web-devicons"
-            "lsp-colors.nvim"
-          ];
-        }
-        nvim-web-devicons
-        lsp-colors-nvim
-      ];
-    };
-  };
-
-  out-of-tree-plugins = {
-    # Don't run neovim for this test, as it's purely to test module evaluation.
-    test.runNvim = false;
-    plugins.lazy = {
-      enable = true;
-      plugins = [
-        # Short or long plugin URL's can be passed via the `__unkeyed` attribute.
-        {
-          "__unkeyed" = "echasnovski/mini.ai";
-          name = "m.ai";
-          enabled = true;
-          version = false;
-        }
-
-        # Short plugin URL's can't be passed via the `url` attribute, unless
-        # it's also passed to the `__unkeyed` attribute.
-        {
-          url = "https://github.com/norcalli/nvim-colorizer.lua";
-          name = "colorizer";
-          enabled = true;
-          version = false;
-        }
-
-        # Short or long plugin URL's can be passed by themselves
-        "ggandor/lightspeed.nvim"
-        "https://github.com/ggandor/leap.nvim"
-        # long URL's can be later referenced by their short URL
-        {
-          __unkeyed = "ggandor/leap.nvim";
-          name = "leap";
-        }
-        # long URL's can be later referenced by their short URL
-        {
-          __unkeyed = "ggandor/leap.nvim";
-          name = "leap";
-        }
-        # Long URL's can also be refernced as is
-        {
-          __unkeyed = "https://github.com/ggandor/leap.nvim";
-          name = "lightspeed";
-        }
-
-        # Plugin names can also be passed by themselves so long as they're
-        # defined somewhere else in the `plugins` list.
-        "oil"
-        {
-          name = "oil";
-          url = "https://github.com/stevearc/oil.nvim";
-        }
-      ];
-    };
-  };
-
-  local-plugin-directory-plugins = {
-    plugins.lazy =
-      with pkgs.vimPlugins;
-      let
-        inherit (pkgs) lib;
-        mkEntryFromDrv = drv: {
-          name = "${lib.getName drv}";
-          path = drv;
-        };
-
-        # Symlink a bunch of test packages to a path in the nix store
-        devPath = pkgs.linkFarm "dev-test-plugins" (
-          map mkEntryFromDrv [
-            nui-nvim
-            vim-vsnip-integ
-            vim-vsnip
-            completion-nvim
-          ]
-        );
-      in
-      {
-        enable = true;
-        settings = {
-          dev = {
-            # Use `devPath` to simulate a local plugin directory path
-            path = "${devPath}";
-            patterns = [ "." ];
-            fallback = false;
-          };
-        };
-
-        plugins = [
-          # Use local plugin that resides in path specified in `devPath` i.e.
-          # `plugins.lazy.settings.dev.path` (See: https://lazy.folke.io/spec)
-          {
-            __unkeyed = "nui.nvim";
-            dev = true;
-          }
-          # local plugins can have dependencies on other plugins
-          {
-            __unkeyed = "completion.nvim";
-            dev = true;
-            dependencies = [
-              {
-                __unkeyed = "vim.vsnip";
-                dev = true;
-              }
-              {
-                __unkeyed = "vim.vsnip.integ";
-                dev = true;
-              }
-            ];
-          }
-        ];
-      };
   };
 }
+
+# disabling_plugins = {
+#   plugins.lazy =
+#     with pkgs.vimPlugins;
+#     let
+#       test_plugin1_path = "${yanky-nvim}";
+#       test_plugin2_path = "${whitespace-nvim}";
+#     in
+#     {
+#       enable = true;
+#       plugins = [
+#         # Enable and then later disable a plugin using it's custom name.
+#         {
+#           name = "test-mini-nvim";
+#           source = mini-nvim;
+#           enabled = true;
+#         }
+#         {
+#           source = "test-mini-nvim";
+#           enabled = false;
+#         }
+#
+#         # Enable and then later disable a plugin using `source`.
+#         {
+#           source = vim-closer;
+#           enabled = true;
+#         }
+#         {
+#           source = vim-closer;
+#           enabled = false;
+#         }
+#
+#         # Enable plugin using `source` and then later disable it using the nix
+#         # package's default name.
+#         {
+#           source = vim-dispatch;
+#           enabled = true;
+#         }
+#         {
+#           source = "vim-dispatch";
+#           enabled = true;
+#         }
+#
+#         # Enable a plugin using it's path given to `source`
+#         {
+#           source = test_plugin1_path;
+#           # We don't need to specify name to be able to disable it later,
+#           # it's just here purely for the sake of the test case.
+#           name = "test_plugin1";
+#           enabled = true;
+#         }
+#         # Disable previously enabled test_plugin1 using `source`.
+#         {
+#           source = test_plugin1_path;
+#           enabled = false;
+#         }
+#
+#         # Enable a plugin using it's path given to `source`, but not giving it a
+#         # custom name.
+#         {
+#           source = test_plugin2_path;
+#           enabled = true;
+#         }
+#         # Disable previously enabled test_plugin2 using `source`.
+#         {
+#           source = test_plugin2_path;
+#           enabled = false;
+#         }
+#       ];
+#
+#     };
+# };
+
+# plugins-with-dependencies = {
+#   plugins.lazy = {
+#     enable = true;
+#     plugins = with pkgs.vimPlugins; [
+#       # Plugins can have dependencies on other plugins
+#       {
+#         source = completion-nvim;
+#         optional = true;
+#         dependencies = [
+#           {
+#             source = vim-vsnip;
+#             optional = true;
+#           }
+#           {
+#             source = vim-vsnip-integ;
+#             optional = true;
+#           }
+#         ];
+#       }
+#
+#       # Use dependency and run lua function after load
+#       {
+#         source = nvim-colorizer-lua;
+#         dependencies = [ nvim-cursorline ];
+#         config = ''
+#           function()
+#             require("nvim-cursorline").setup{}
+#           end '';
+#       }
+#
+#       # Dependencies can be a single package
+#       {
+#         source = LazyVim;
+#         dependencies = trouble-nvim;
+#       }
+#
+#       # Dependencies can be multiple packages
+#       {
+#         source = nvim-cmp;
+#         dependencies = [
+#           cmp-cmdline
+#           cmp-vsnip
+#         ];
+#       }
+#
+#       # Dependencies can be a single name that is defined elsewhere
+#       {
+#         source = nvim-autopairs;
+#         dependencies = "luasnip";
+#       }
+#       {
+#         name = "luasnip";
+#         source = luasnip;
+#       }
+#
+#       # Dependencies can be a list of names that are defined elsewhere
+#       {
+#         source = nvim-lightbulb;
+#         dependencies = [
+#           "nvim-lspconfig"
+#           "cmp-nvim-lua"
+#           "cmp-nvim-lsp"
+#         ];
+#       }
+#       {
+#         source = nvim-lspconfig;
+#         name = "nvim-lspconfig";
+#       }
+#       {
+#         source = cmp-nvim-lua;
+#         name = "cmp-nvim-lua";
+#       }
+#       {
+#         source = cmp-nvim-lsp;
+#         name = "cmp-nvim-lsp";
+#       }
+#
+#       # Dependencies can be a list of names that are defined elsewhere in the
+#       # list of plugins. If the names given are just the default names of a
+#       # package they need not be explicitly defined.
+#       {
+#         source = nui-nvim;
+#         dependencies = [
+#           "nvim-web-devicons"
+#           "lsp-colors.nvim"
+#         ];
+#       }
+#       nvim-web-devicons
+#       lsp-colors-nvim
+#     ];
+#   };
+# };
+
+# local-plugin-sourceectory-plugins = {
+#   plugins.lazy =
+#     with pkgs.vimPlugins;
+#     let
+#       inherit (pkgs) lib;
+#       mkEntryFromDrv = drv: {
+#         name = "${lib.getName drv}";
+#         path = drv;
+#       };
+#
+#       # Symlink a bunch of test packages to a path in the nix store
+#       devPath = pkgs.linkFarm "dev-test-plugins" (
+#         map mkEntryFromDrv [
+#           nui-nvim
+#           vim-vsnip-integ
+#           vim-vsnip
+#           completion-nvim
+#         ]
+#       );
+#     in
+#     {
+#       enable = true;
+#       settings = {
+#         dev = {
+#           # Use `devPath` to simulate a local plugin sourceectory path
+#           path = "${devPath}";
+#           patterns = [ "." ];
+#           fallback = false;
+#         };
+#       };
+#
+#       plugins = [
+#         # Use local plugin that resides in path specified in `devPath` i.e.
+#         # `plugins.lazy.settings.dev.path` (See: https://lazy.folke.io/spec)
+#         {
+#           source = "nui.nvim";
+#           dev = true;
+#         }
+#         # local plugins can have dependencies on other plugins
+#         {
+#           source = "completion.nvim";
+#           dev = true;
+#           dependencies = [
+#             {
+#               source = "vim.vsnip";
+#               dev = true;
+#             }
+#             {
+#               source = "vim.vsnip.integ";
+#               dev = true;
+#             }
+#           ];
+#         }
+#       ];
+#     };
+# };
+# }
