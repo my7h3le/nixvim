e7e78dd00655fe4243d85fd90e6f8bd432100a3d
Tash <mandate-word-dupe@duck.com>
2024-09-13 04:41:02 -0400

    test use no ${}

--- plugins/pluginmanagers/lazy.nix
+++ plugins/pluginmanagers/lazy.nix
@@ -280,19 +280,16 @@ lib.nixvim.neovim-plugin.mkNeovimPlugin {
       let
         pluginToSpec =
           plugin:
-          if ((plugin.source or null) != null) then
-            (
-              lib.removeAttrs plugin [ "source" ]
-              // lib.optionalAttrs (lib.isDerivation plugin.source) { dir = "${plugin.source}"; }
-              // lib.optionalAttrs (lib.isPath plugin.source) { dir = "${plugin.source}"; }
-              // lib.optionalAttrs (isShortGitURL plugin.source) { __unkeyed = plugin.source; }
-              // lib.optionalAttrs (isGitURL plugin.source) { url = plugin.source; }
-              // lib.optionalAttrs ((plugin.dependencies or null) != null) {
-                dependencies = map pluginToSpec plugin.dependencies;
-              }
-            )
-          else
-            null;
+          lib.mkIf ((plugin.source or null) != null) (
+            lib.removeAttrs plugin [ "source" ]
+            // lib.optionalAttrs (lib.isDerivation plugin.source) { dir = "${plugin.source}"; }
+            // lib.optionalAttrs (lib.isPath plugin.source) { dir = plugin.source; }
+            // lib.optionalAttrs (isShortGitURL plugin.source) { __unkeyed = plugin.source; }
+            // lib.optionalAttrs (isGitURL plugin.source) { url = plugin.source; }
+            // lib.optionalAttrs ((plugin.dependencies or null) != null) {
+              dependencies = map pluginToSpec plugin.dependencies;
+            }
+          );
       in
       map pluginToSpec cfg.plugins;
   };
